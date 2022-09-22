5403b3fb69da2f33a755433a7c0a386cfd31f482
dosmaster4life <jefferysuperman@gmail.com>
2022-09-22 12:16:14 -0600

    swagger implementation

---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'Use Contacts API to manage your contacts'
+  },
+  host: 'cse04contacts.onrender.com',
+  schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json file
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
